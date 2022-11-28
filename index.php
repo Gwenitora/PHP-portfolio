f9a74cf35253e468b44b926a725c15dd82ee704c
Gwenitora <gwenitora.gardu74@gmail.com>
2022-11-28 12:41:02 +0100

    feature: include

--- index.php
+++ index.php
@@ -1,91 +1,10 @@
-<!DOCTYPE html>
+<?php
+include 'include.php';
+?>
 
-<html lang="fr">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width">
-  <meta name="description" content="Page Acceuil du portfolio de Gwendal Acquart--Reylans et de Ewen Beaufils.">
-  <link rel="shortcut icon" href="#">
-  <title>Portfolio Gwendal et Ewen</title>
-  <link rel="shortcut icon" href="img/title.ico">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
-  <link href="./css/style.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-</head>
-
-<body class="color perso-1">
-  <!-- NavBar -->
-  <nav class="color perso-2 z-depth-0">
-    <div class="nav-wrapper container">
-      <a href="" class="brand-logo">Portfolio</a>
-      <a href="#" data-target="mobile-links" class="sidenav-trigger">
-        <i class="material-icons">menu</i>
-      </a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href=""><i class="material-icons left">home</i>Home</a></li>
-        <li><a href="#contact" class="modal-trigger"><i class="material-icons left">phone</i>Contact</a> </li>
-        <li><a href="#" class="dropdown-trigger" data-target="dropdown-team"><i
-              class="material-icons left">group</i>Team</a> </li>
-      </ul>
-
-      <!-- Dropdown menu -->
-      <ul id="dropdown-team" class="dropdown-content color perso-2">
-        <li><a href="#team" class="white-text">Team</a></li>
-        <li><a href="html/info-gwendal.html" class="white-text">Gwendal</a></li>
-        <li><a href="html/info-ewen.html" class="white-text">Ewen</a></li>
-      </ul>
-    </div>
-  </nav>
-  <header>
-    <div class="parallax-container">
-      <div class="parallax"><img src="img/image-nav-bar.jpg" alt="Voie lactée en parallaxe"></div>
-    </div>
-  </header>
-  <ul class="sidenav color perso-2" id="mobile-links">
-    <li><a href="" onclick="sidenavClose()" class="white-text"><i class="material-icons left">home</i>Home</a> </li>
-    <li><a href="#team" onclick="sidenavClose()" class="white-text"><i class="material-icons left">phone</i>Team</a></li>
-    <li><a href="#contact" onclick="sidenavClose()" class="modal-trigger white-text"><i class="material-icons left">group</i>Contact</a></li>
-  </ul>
-
-
-  <!-- Contact -->
-  <div class="container">
-    <div class="modal color perso-2 border-radius" id="contact">
-
-      <div class="row">
-        <div class="input-field col s12 l5">
-          <i class="material-icons prefix">account_circle</i>
-          <input type="text" id="name-input" class="name">
-          <label for="name-input">NOM Prénom</label>
-        </div>
-        <div class="input-field col s12 l7">
-          <i class="material-icons prefix">mail</i>
-          <input type="email" id="email-input" class="email">
-          <label for="email-input">Email</label>
-        </div>
-        <div class="input-field col s12">
-          <i class="material-icons prefix">star</i>
-          <input type="text" id="object-input" class="object">
-          <label for="object-input">Sujet</label>
-        </div>
-        <div class="input-field col s12">
-          <i class="material-icons prefix">textsms</i>
-          <textarea id="message-input" class="message materialize-textarea"></textarea>
-          <label for="message-input">Message</label>
-        </div>
-      </div>
-      <div class="modal-close color perso-2 center" id="suprise">
-        <button class="btn waves-effect waves-light">
-          <span>Envoyer</span>
-          <i class="material-icons right">send</i>
-        </button>
-      </div>
-
-    </div>
-  </div>
+  <?php
+  headerPage();
+  ?>
 
   <!-- Team -->
   <div class="section" id="team">
@@ -143,28 +62,5 @@
     </div>
   </div>
 
-
-  <!-- Footer -->
-  <footer class="page-footer color perso-2">
-    <div class="footer-copyright">
-      <div class="container">
-        <p class="left">&copy; 2022 Copyright ACQUART--REYLANS Gwendal</p>
-        <p class="right">&copy; 2022 Copyright BEAUFILS Ewen</p>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Easter Egg -->
-  <div class="easter-gif border-radius">
-    <img src="./img/hastley-rick.gif" alt="you rick rolled">
-  </div>
-
-  <!-- Scripts js -->
-  <script src="https://kit.fontawesome.com/63d128608a.js" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.6.1.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-  <script src="./scripts/script.js"></script>
-
-</body>
-
-</html>
+  <?php
+  footerPage();
