883da19330c8d3eb8ea1fba6d6b34ce9927c0c4b
EwenBeaufils <ewen.beaufils09@gmail.com>
2022-11-29 11:41:54 +0100

    feature : actions

---
+++ action/action.php
@@ -0,0 +1,18 @@
+<div class="container">
+      <div class="modal color perso-2 border-radius" id="contact">
+        <div class="row">
+          <div class="input-field col s12 l5">
+            <i class="material-icons prefix">account_circle</i>
+            <input type="submit" value = "Se connecter">
+          </div>
+          <div class="input-field col s12 l7">
+            <i class="material-icons prefix">mail</i>
+            <input type="submit" value = "Se déconnecter">
+          </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">lock</i>
+            <input type="submit" value = "Suprimer son compte">
+          </div>
+        </div>
+      </div>
+    </div>
