54a8fee28e6aee70bbb5e78f5d20bd894cee64a9
EwenBeaufils <ewen.beaufils09@gmail.com>
2022-11-30 22:02:06 +0100

    feature : index

--- admin/upload_file.php
+++ admin/upload_file.php
@@ -2,8 +2,9 @@
 //sauvegarder le fichier dans un dossier spécifique
 if ($_FILES['img']['name'] != 'upload/') {
     $destination = "upload/".$_FILES['img']['name']; //dossier "upload"
-    move_uploaded_file($_FILES['img']['tmp_name'],$destination);
-} else {
-    $destination = "NULL";
-}
+    move_uploaded_file($_FILES['img']['tmp_name'],"../".$destination);
+    
+
+
+
 ?>
