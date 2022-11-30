11ccb9c5811e5041ad1f980b164a79b2402cc262
EwenBeaufils <ewen.beaufils09@gmail.com>
2022-11-30 14:48:16 +0100

    feature : green toast

--- action/logout.php
+++ action/logout.php
@@ -2,6 +2,10 @@
 
 require_once "config.php";
 
+$_SESSION['toast'][] = [
+    'text' => 'Bienvenue'.$_SESSION['user']['name'] ,
+    'classes' => $greenToast
+];
 unset($_SESSION["user"]);
 
 require_once "go-back.php";
