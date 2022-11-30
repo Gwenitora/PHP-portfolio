64d855bcd21b1552afda5b83d7bab454ced3a280
Gwenitora <gwenitora.gardu74@gmail.com>
2022-11-30 22:11:44 +0100

    debug: optimisation

--- admin/upload_file.php
+++ admin/upload_file.php
@@ -2,9 +2,6 @@
 //sauvegarder le fichier dans un dossier spécifique
 if ($_FILES['img']['name'] != 'upload/') {
     $destination = "upload/".$_FILES['img']['name']; //dossier "upload"
-    echo "<pre>";
-    print_r($_FILES['img']['tmp_name']);
-    echo "</pre>";
     move_uploaded_file($_FILES['img']['tmp_name'],"../".$destination);
 } else {
     $destination = "NULL";
