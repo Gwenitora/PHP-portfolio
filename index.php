b4582cd6469d865d3246a294aa94038cdb66e4e8
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-01 15:56:37 +0100

    feature: identity card

--- index.php
+++ index.php
@@ -13,21 +13,21 @@ $a = count($data)
       <div class="container">
         <h1 class="center animate__animated animate__rubberBand">Team</h1>
           <div class="row">
-            <?php foreach($data as $var) {
-              if($var["as_portfolio"] == 1){?>
+            <?php foreach($data as $user) {
+              if($user["as_portfolio"] == 1){?>
                 <!-- Team -->
 
                 <div class="col s12 m6">
                   <div class="card color perso-2 border-radius">
                     <div class="card-image">
-                      <img src="<?php echo $var['img'] ?>" alt="Photo de <?php echo $var['name'] ?>" class="animate__animated animate__rotateInDownLeft">
-                      <a href="./info-gwendal.php" class="halfway-fab btn-floating pulse orange">
+                      <img src="<?php echo $user['img'] ?>" alt="Photo de <?php echo $user['name'] ?>" class="animate__animated animate__rotateInDownLeft">
+                      <a href="./identity-card.php?id=<?= $user['id'] ?>" class="halfway-fab btn-floating pulse orange">
                         <i class="material-icons">check</i>
                       </a>
                     </div>
                     <div class="card-content">
-                      <span class="card-title"><?php echo $var['name']?></span>                                        
-                      <p> <?php echo $var['description'] ?></p>
+                      <span class="card-title"><?php echo $user['name']?></span>                                        
+                      <p> <?php echo $user['description'] ?></p>
                     </div>
                     <div class="card-action">
                       <a href="./gang-beasts.php">Gang Beasts</a>
