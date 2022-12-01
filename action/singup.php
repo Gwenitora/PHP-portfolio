a27463dbb4d62d638e7e8481a963ddca61838bd3
Gwenitora <gwenitora.gardu74@gmail.com>
2022-12-01 15:31:03 +0100

    feature: identity-card

--- action/singup.php
+++ action/singup.php
@@ -28,23 +28,27 @@ $preEmail = $pdo->prepare($sqlEmail);
 $preEmail->execute($dataBindedEmail);
 $dataEmail = $preEmail->fetchAll(PDO::FETCH_ASSOC);
 
+$sql = "INSERT INTO users(name, password, email) VALUES(:name, SHA1(:password), :email)";
+$pre = $pdo->prepare($sql);
+
 /* Correction erreur dans modal sign up */
 function checkPassword($pwd,$email, $name, $dataName, $dataEmail) {
+	$retourn = true;
 
     if (strlen($pwd) < 8) {
 		$_SESSION['toast'][] =[
 			"text" => "Mot de passe trop court",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     }
 
 	if (strlen($email) == 0) {
 		$_SESSION['toast'][] =[
-			"text" => "Email trop",
+			"text" => "Email trop court",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     }
 
 	if (strlen($name) == 0) {
@@ -52,7 +56,7 @@ function checkPassword($pwd,$email, $name, $dataName, $dataEmail) {
 			"text" => "Nom d'utilisateur trop court",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     }
 
     if (!preg_match("#[0-9]+#", $pwd)) {
@@ -60,7 +64,7 @@ function checkPassword($pwd,$email, $name, $dataName, $dataEmail) {
 			"text" => "Le mot de passe doit contenir au moins un chiffre",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     }
 
     if (!preg_match("#[a-zA-Z]+#", $pwd)) {
@@ -68,15 +72,15 @@ function checkPassword($pwd,$email, $name, $dataName, $dataEmail) {
 			"text" => "Le mot de passe doit contenir au moins une lettre",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     } 
 
 	if (!count($dataName) == 0 ) {
 		$_SESSION['toast'][] =[
-			"text" => "Ce nom d'utilisateur est déjà utilisé",
+			"text" => "Ce nom est déjà utilisé",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
     } 
 	
 	if (!count($dataEmail) == 0) {
@@ -84,27 +88,23 @@ function checkPassword($pwd,$email, $name, $dataName, $dataEmail) {
 			"text" => "Cet email est déjà utilisé",
 			"classes" => $_SESSION["toastConfig"]["redToast"],
 		];
-		return false;
+		$retourn = false;
 	}
 
-	return true;
+	return $retourn;
 }
 
 
-if (!checkPassword($_POST['password'], $_POST['email'], $_POST['name'], $dataName, $dataEmail)) {
+if (!checkPassword($_POST['password'], $_POST['email'], $_POST['name'], $dataName, $dataEmail, $dataSign)) {
 	$_SESSION['modal'] = "signUp";
 }else{
-	$sql = "INSERT INTO users(name, password, email) VALUES(:name, SHA1(:password), :email)";
-	$pre = $pdo->prepare($sql);
 	$pre->execute($dataBinded);
-	$_SESSION['user'] = $pre->fetch(PDO::FETCH_ASSOC);
+	$_SESSION['user'] = $pre->fetch(PDO::FETCH_ASSOC);;
 	$_SESSION['toast'][] = [
         'text' => 'Compte de  '.$_SESSION['user']['name']. " créé" ,
         'classes' => $_SESSION["toastConfig"]["greenToast"]
     ];
 }
-
-
 
 require_once "go-back.php";
 ?>
